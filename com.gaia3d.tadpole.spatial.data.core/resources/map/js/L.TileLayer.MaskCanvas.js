f33d3f6e3843c48265dd86efbb9352e52d953ce9
BJ Jang <qgis.tr.kr@gmail.com>
2014-11-22 16:14:31 +0900

    라인 및 폴리곤 그리기 1차 완료. 성능개선 필요함

--- com.gaia3d.tadpole.spatial.data.core/resources/map/js/L.TileLayer.MaskCanvas.js
+++ com.gaia3d.tadpole.spatial.data.core/resources/map/js/L.TileLayer.MaskCanvas.js
@@ -1,5 +1,5 @@
 L.TileLayer.MaskCanvas = L.TileLayer.Canvas.extend({
-    options: {
+/*    options: {
         radius: 5,
         useAbsoluteRadius: true,  // true: radius in meters, false: radius in pixels
         color: '#000',
@@ -8,6 +8,20 @@ L.TileLayer.MaskCanvas = L.TileLayer.Canvas.extend({
         lineColor: undefined,  // color of the circle outline if noMask is true
         debug: false
     },
+*/
+    options: {
+    	tileSize: 512,
+        opacity: 1.0,
+        noMask: true,  // true results in normal (filled) circled, instead masked circles
+        pointColor: 'rgba(0, 0, 255, 0.5)',  // color of the circle outline if noMask is true
+        radius: 10,
+        useAbsoluteRadius: true,  // true: radius in meters, false: radius in pixels
+        lineColor: 'rgba(0, 0, 255, 0.5)',  // color of the circle outline if noMask is true
+        lineWidth: 2,
+        fillColor: 'rgba(0, 0, 200, 0.5)',
+        debug: false
+    },
+    _flagOnLoad: false,
 
     initialize: function (options, data) {
         var self = this;
@@ -49,84 +63,74 @@ L.TileLayer.MaskCanvas = L.TileLayer.Canvas.extend({
     setData: function(dataset) {
         var self = this;
 
-        if (dataset instanceof L.GeoJSON) {
-        	this.bounds = dataset.getBounds();
-            this._quad = new QuadTree(this._boundsToQuery(this.bounds), false, 6, 6);
-        	dataset.getLayers().forEach(function(d) {
-                self._quad.insert({
-                    x: d.feature.geometry.coordinates[0], 
-                    y: d.feature.geometry.coordinates[1] 
-                });
-            });
-        }
-        else if (dataset instanceof Array) {
-            this.bounds = new L.LatLngBounds(dataset);
-            this._quad = new QuadTree(this._boundsToQuery(this.bounds), false, 6, 6);
-            var first = dataset[0];
-            var xc = 1, yc = 0;
-            if (first instanceof L.LatLng) {
-                xc = "lng";
-                yc = "lat";
-            }
-
-            dataset.forEach(function(d) {
-                self._quad.insert({
-                    x: d[xc], //lng
-                    y: d[yc] //lat
-                });
-            });
-        }
-
-
-        if (this._map) {
-            this.redraw();
-        }
+        self.bounds = null;
+        self._quad = null;
+        
+        self.addData(dataset);
     },
 
     addData: function(dataset) {
         var self = this;
 
-        if (dataset instanceof L.GeoJSON) {
-        	var subBounds = dataset.getBounds();
-        	if (!self.bounds) 
-        		self.bounds = subBounds;
-        	else 
-        		self.bounds.extend(subBounds);
-        	
-        	if (!self._quad) 
-        		self._quad = new QuadTree(this._boundsToQuery(self.bounds), false, 6, 6);
-        	else {
-        		tempQuad = new QuadTree(this._boundsToQuery(self.bounds), false, 6, 6);
-        		tempQuad.insert(self._quad.root.getChildren());
-        		self._quad = tempQuad;
-        	}
-        	
-        	dataset.getLayers().forEach(function(d) {
-                self._quad.insert({
-                    x: d.feature.geometry.coordinates[0], 
-                    y: d.feature.geometry.coordinates[1] 
+        try {
+            self._flagOnLoad = true;
+            if (dataset instanceof L.GeoJSON) {
+            	var subBounds = dataset.getBounds();
+            	if (!self.bounds) 
+            		self.bounds = subBounds;
+            	else 
+            		self.bounds.extend(subBounds);
+            	
+            	if (!self._quad) 
+            		self._quad = new QuadTree(this._boundsToQuery(self.bounds), false, 6, 6);
+            	else {
+            		tempQuad = new QuadTree(this._boundsToQuery(self.bounds), false, 6, 6);
+            		tempQuad.insert(self._quad.root.getChildren());
+            		self._quad = tempQuad;
+            	}
+            	
+            	dataset.getLayers().forEach(function(d) {
+            		if (d.feature && d.feature.geometry && d.feature.geometry.type == "Point") {
+    	                self._quad.insert({
+    	                    x: d.getLatLng().lng, 
+    	                    y: d.getLatLng().lat,
+    	                    geometry: d.feature.geometry
+    	                });
+            		}
+            		else {
+            			bounds = d.getBounds();
+    	                self._quad.insert({
+    	                    x: bounds.getWest(), 
+    	                    y: bounds.getSouth(),
+    	                    width: bounds.getEast() - bounds.getWest(),
+    	                    height:	bounds.getNorth() - bounds.getSouth(),
+    	                    geometry: d.feature.geometry
+    	                });
+            		}
                 });
-            });
-        }
-        else if (dataset instanceof Array) {
-//            this.bounds = new L.LatLngBounds(dataset);
-//            this._quad = new QuadTree(this._boundsToQuery(this.bounds), false, 6, 6);
-//            var first = dataset[0];
-//            var xc = 1, yc = 0;
-//            if (first instanceof L.LatLng) {
-//                xc = "lng";
-//                yc = "lat";
-//            }
-//
-//            dataset.forEach(function(d) {
-//                self._quad.insert({
-//                    x: d[xc], //lng
-//                    y: d[yc] //lat
-//                });
-//            });
-        }
+            }
+            else if (dataset instanceof Array) {
+                this.bounds = new L.LatLngBounds(dataset);
+                this._quad = new QuadTree(this._boundsToQuery(this.bounds), false, 6, 6);
+                var first = dataset[0];
+                var xc = 1, yc = 0;
+                if (first instanceof L.LatLng) {
+                    xc = "lng";
+                    yc = "lat";
+                }
 
+                dataset.forEach(function(d) {
+                    self._quad.insert({
+                        x: d[xc], //lng
+                        y: d[yc] //lat
+                    });
+                });
+            }
+		} catch(err) {
+			console.log("Rise exception(addData function) : " + err);
+		}
 
+        self._flagOnLoad = false;
         if (this._map) {
             this.redraw();
         }
@@ -148,34 +152,6 @@ L.TileLayer.MaskCanvas = L.TileLayer.Canvas.extend({
         var x = Math.round(p.x - s.x);
         var y = Math.round(p.y - s.y);
         return [x, y];
-    },
-
-    _drawPoints: function (ctx, coordinates) {
-        var c = ctx.canvas,
-            g = c.getContext('2d'),
-            self = this,
-            p,
-            tileSize = this.options.tileSize;
-        g.fillStyle = this.options.color;
-
-        if (this.options.lineColor) {
-          g.strokeStyle = this.options.lineColor;
-          g.lineWidth = this.options.lineWidth || 1;
-        }
-        g.globalCompositeOperation = 'source-over';
-        if (!this.options.noMask) {
-            g.fillRect(0, 0, tileSize, tileSize);
-            g.globalCompositeOperation = 'destination-out';
-        }
-        coordinates.forEach(function(coords) {
-            p = self._tilePoint(ctx, coords);
-            g.beginPath();
-            g.arc(p[0], p[1], self._getRadius(), 0, Math.PI * 2);
-            g.fill();
-            if (self.options.lineColor) {
-                g.stroke();
-            }
-        });
     },
 
     _boundsToQuery: function(bounds) {
@@ -215,31 +191,167 @@ L.TileLayer.MaskCanvas = L.TileLayer.Canvas.extend({
     },
 
     _draw: function (ctx) {
-        if (!this._quad || !this._map) {
-            return;
+    	try {
+        	if (this._flagOnLoad)
+        		return;
+        	
+            if (!this._quad || !this._map) {
+                return;
+            }
+
+            var tileSize = this.options.tileSize;
+
+            var nwPoint = ctx.tilePoint.multiplyBy(tileSize);
+            var sePoint = nwPoint.add(new L.Point(tileSize, tileSize));
+
+            if (this.options.useAbsoluteRadius) {
+                var centerPoint = nwPoint.add(new L.Point(tileSize/2, tileSize/2));
+                this._latlng = this._map.unproject(centerPoint);
+                this.projectLatlngs();
+            }
+
+            // padding
+            var pad = new L.Point(this._getRadius(), this._getRadius());
+            nwPoint = nwPoint.subtract(pad);
+            sePoint = sePoint.add(pad);
+
+            var bounds = new L.LatLngBounds(this._map.unproject(sePoint), this._map.unproject(nwPoint));
+
+//            var nodes = this._quad.retrieveInBounds(this._boundsToQuery(bounds));
+            var nodes = this._quad.retrieve(this._boundsToQuery(bounds));
+
+            this._drawNodes(ctx, nodes);
+    	} catch(err) {
+    		console.log("Rise exception(_draw function) : " + err);
+    	}
+    	
+    },
+
+    _drawNodes: function (ctx, nodes) {
+        var c = ctx.canvas,
+            g = c.getContext('2d'),
+            self = this,
+            p,
+            tileSize = this.options.tileSize;
+        g.fillStyle = this.options.fillColor;
+
+        if (this.options.lineColor) {
+          g.strokeStyle = this.options.lineColor;
+          g.lineWidth = this.options.lineWidth || 1;
         }
-
-        var tileSize = this.options.tileSize;
-
-        var nwPoint = ctx.tilePoint.multiplyBy(tileSize);
-        var sePoint = nwPoint.add(new L.Point(tileSize, tileSize));
-
-        if (this.options.useAbsoluteRadius) {
-            var centerPoint = nwPoint.add(new L.Point(tileSize/2, tileSize/2));
-            this._latlng = this._map.unproject(centerPoint);
-            this.projectLatlngs();
+        g.globalCompositeOperation = 'source-over';
+        if (!this.options.noMask) {
+            g.fillRect(0, 0, tileSize, tileSize);
+            g.globalCompositeOperation = 'destination-out';
         }
+        nodes.forEach(function(node) {
+        	if (typeof(node.geometry) == "undefined") {
+				p = self._tilePoint(ctx, node);
+				g.beginPath();
+				g.arc(p[0], p[1], self._getRadius(), 0, Math.PI * 2);
+				g.fill();
+				if (self.options.lineColor) {
+				    g.stroke();
+				}
+				return;
+        	}
+        	
+        	var geometry = node.geometry;
+        	var type = geometry.type;
+        	switch (type) {
+        	case "Point":
+        		pnt = geometry.coordinates;
+        		self._drawPoint(ctx, g, pnt);
+        		break;
+        	case "MultiPoint":
+        		if (geometry.coordinates.length == 1) { // forEach 동작이상 대응
+        			self._drawPoint(ctx, g, geometry.coordinates[0]);
+        		}
+        		else {
+            		geometry.coordinates.forEach(function(multipnt) {
+            			multipnt.forEach(function(pnt) {
+            				self._drawPoint(ctx, g, pnt);
+            			});
+            		});
+        		}
+        		break;
+        	case "Polygon":
+        		poly = geometry.coordinates;
+        		self._drawPolygon(ctx, g, poly);
+        		break;
+        	case "MultiPolygon":
+        		geometry.coordinates.forEach(function(multipoly) {
+        			multipoly.forEach(function(poly) {
+        				self._drawPolygon(ctx, g, poly);
+        			});
+        		});
+        		break;
+        	case "LineString":
+        		line = geometry.coordinates;
+        		self._drawLineString(ctx, g, line);
+        		break;
+        	case "MultiLineString":
+        		geometry.coordinates.forEach(function(multiline) {
+        			multiline.forEach(function(line) {
+        				self._drawLineString(ctx, g, line);
+        			});
+        		});
+        		break;
+        	}
+        });
+    },
 
-        // padding
-        var pad = new L.Point(this._getRadius(), this._getRadius());
-        nwPoint = nwPoint.subtract(pad);
-        sePoint = sePoint.add(pad);
+    _drawPoint: function(ctx, g, pnt) {
+    	self = this;
+    	p = self._tilePoint(ctx, {x:pnt[0],y:pnt[1]});
+		g.beginPath();
+		g.arc(p[0], p[1], self._getRadius(), 0, Math.PI * 2);
+		g.fill();
+		if (this.options.lineColor) {
+		   g.stroke();
+		}
+    },
+    _drawLineString: function(ctx, g, line) {
+    	self = this;
+    	var isStart = true;
+    	var p;
+    	
+		g.beginPath();
+		line.forEach(function(pnt) {
+            p = self._tilePoint(ctx, {x:pnt[0],y:pnt[1]});
+            if(isStart) {
+            	g.moveTo(p[0], p[1]);
+            	isStart = false;
+            } else {
+                g.lineTo(p[0], p[1]);
+            }
+		});
+        g.stroke();
+    },
+    _drawPolygon: function(ctx, g, polygon) {
+    	self = this;		
+		var startPnt = null;
+		var p;
+		
+		g.beginPath();
+		polygon.forEach(function(pnt) {
+			p = self._tilePoint(ctx, {x:pnt[0],y:pnt[1]});
+			if (!startPnt) {
+				startPnt = pnt;
+				g.moveTo(p[0], p[1]);
+				return;
+			}
 
-        var bounds = new L.LatLngBounds(this._map.unproject(sePoint), this._map.unproject(nwPoint));
-
-        var coordinates = this._quad.retrieveInBounds(this._boundsToQuery(bounds));
-
-        this._drawPoints(ctx, coordinates);
+			g.lineTo(p[0], p[1]);
+			
+			if (pnt[0] == startPnt[0] && pnt[1] == startPnt[1]) {
+				startPnt = null;
+			}
+		});
+		if (startPnt) g.lineTo(p[0], p[1]);
+		g.closePath();
+        g.fill();
+        g.stroke();
     }
 });
 
