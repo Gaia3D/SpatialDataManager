1da87b7bfbc59d70ee981e34bd660212c9bfc1fd
BJ Jang <qgis.tr.kr@gmail.com>
2014-11-25 11:35:01 +0900

    heatmap 메모리 안정화

--- com.gaia3d.tadpole.spatial.data.core/resources/map/js/tadpoleSdm.js
+++ com.gaia3d.tadpole.spatial.data.core/resources/map/js/tadpoleSdm.js
@@ -31,13 +31,7 @@ options.heatmapOptions = {
 		  // if set to false the heatmap uses the global maximum for colorization
 		  // if activated: uses the data maximum within the current map boundaries 
 		  //   (there will always be a red spot with useLocalExtremas true)
-		  "useLocalExtrema": true,
-		  // which field name in your data represents the latitude - default "lat"
-		  latField: 'lat',
-		  // which field name in your data represents the longitude - default "lng"
-		  lngField: 'lng',
-		  // which field name in your data represents the data value - default "value"
-		  valueField: 'count'
+		  "useLocalExtrema": true
 		};
 
 
